529e47959de9e175a2eaf36f61f440fdf9d3b3bd
Adam Strautman <adam.strautman@gmail.com>
2018-02-04 14:20:35 -0800

    Added email functionality to website. created placeholder config file email.php. MAKE SURE TO UPDATE IF PULLING THIS FILE.

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config = array(
+	'protocol' => 'smtp',
+	'smtp_host' => 'ssl://secure152.inmotionhosting.com',
+	'smtp_port' => '465',
+	'smtp_timeout' => '7',
+	'smtp_user' => CHANGEME,
+	'smtp_pass' => CHANGEME,
+	'charset' => 'iso-8859-1',
+	'newline' => "\r\n",
+	'mailtype' => 'html',
+);
