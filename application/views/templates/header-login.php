97c05c23bfd4b3a239cb4c03586cea54e655be8d
Adam Strautman <adam.strautman@gmail.com>
2018-02-04 15:19:48 -0800

    Updated Login header to match site name

--- application/views/templates/header-login.php
+++ application/views/templates/header-login.php
@@ -9,13 +9,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <title>Route Review - Login</title>
 
     <!-- Bootstrap Core CSS -->
     <link href='<?php echo base_url("assets/vendor/bootstrap/css/bootstrap.min.css"); ?>' rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href='<?php echo base_url("assets/vendor/metisMenu/metisMenu.min.css"); ?>' rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href='<?php echo base_url("assets/dist/css/sb-admin-2.css"); ?>' rel="stylesheet">
