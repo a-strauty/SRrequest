12f17a96749264380db9dc1a4e3074bb74938457
Adam Strautman <adam.strautman@gmail.com>
2018-02-09 20:01:35 -0800

    Removed Boostrap by deleting Assets folder and removing references in header and footer template files.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,10 +1,6 @@
 <html>
         <head>
                 <title><?php echo $title; ?></title>
-
-                <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-                <script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-3.1.0.min.js"); ?>" defer></script>
-                <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>" defer></script>
         </head>
         <body>
 
