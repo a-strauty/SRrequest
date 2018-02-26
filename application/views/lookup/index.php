c8a3c7400c8b817214ef8ee915c167af5a8160eb
Adam Strautman <adam.strautman@gmail.com>
2018-02-26 07:29:05 -0800

    Styled the Stops by day form.

--- application/views/lookup/index.php
+++ application/views/lookup/index.php
@@ -1,43 +1,49 @@
-<div id="form_container">
-
+<div class="grid-title">
 	<h2>Stops by Day</h2>
-
-	<form id="form_srbyday" class="srbyday"  method="get" action="sr/results">
-	<ul>
-	<li id="li1">
-	<!-- create dropdown -->
-	<label class="description" for="element_1">Select Sales-Rep</label>
-		<span>
-			<select name="sr_select">
-				<?php
-				//use php function to create sales rep dropdown
-				foreach($names as $row)
-				{
-					echo '<option value="'.$row->sr_id.'">'.$row->sr_name.'</option>';
-				}
-				?>
-			</select>
-		</span>
-	</li>
-	<li "li2">
-	<label class="description" for="element_1">Select Day</label>
-		<span>
-            <?php
-            //use php to create the day bubble options
-            foreach($days as $row)
-            {
-				echo "<input id=".$row->day." name='dayradio' class='element radio' type='radio' value=".$row->value." />";
-				echo "<label class='choice' for=".$row->day.">".$row->day."</label>";
-			}
-            ?>
-		</span>
-	</li>
-	<li>
-		<input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
-	</li>
-	</ul>
-	</form>
 </div>
-<p>
-<p><?php echo anchor('', 'Back')?>
-<p>
+
+	<form class="grid-form" id="srbyday"  method="get" action="sr/results">
+
+		<div class="subgrid-formdropdown">
+	<!-- create dropdown -->
+			<label class="description" for="element_1">Who are you?</label>
+
+				<span>
+					<select name="sr_select">
+						<?php
+						//use php function to create sales rep dropdown
+						foreach($names as $row)
+						{
+						echo '<option value="'.$row->sr_id.'">'.$row->sr_name.'</option>';
+						}
+						?>
+					</select>
+				</span>
+
+		</div>
+
+		<div class="subgrid-formDaySelectDescription">
+			<label class="description" for="element_1">Select the day you would like to change</label>
+		</div>
+		<div class="subgrid-formDaySelect">
+				<span>
+						<?php
+						//use php to create the day bubble options
+						foreach($days as $row)
+						{
+						echo "<input id=".$row->day." name='dayradio' class='element radio' type='radio' value=".$row->value." />";
+						echo "<label class='choice' for=".$row->day.">".$row->day."</label>";
+						}
+						?>
+				</span>
+		</div>
+		<div class="subgrid-formDaySelectSubmit">
+			<input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
+		</div>
+
+	</form>
+
+
+<div class="grid-bottomleft">
+	<?php echo anchor('', 'Back')?>
+</div>
