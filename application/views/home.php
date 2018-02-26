b3aca2db181758523aacffa6d792df303a69c81c
Adam Strautman <adam.strautman@gmail.com>
2018-02-26 06:48:28 -0800

    Restyled the Homepage. Reclassified the containing objects the anchors to be buttons.

--- application/views/home.php
+++ application/views/home.php
@@ -1,11 +1,11 @@
-<div class="box nav_button" id="nav_lookup">
-   <?php echo anchor('sr', 'Lookup route and Create a request'); ?>
+<div class="grid-item">
+   <?php echo anchor('sr', 'Create a request as a<h2>Sales Rep</h2>Click Here', 'class="nav-button" id="nav-review"'); ?>
 </div>
 
-<div class="box nav_button" id="nav_review">
-   <?php echo anchor('review', 'Review and track previously made requests'); ?>
+<div class="grid-item">
+   <?php echo anchor('review', 'Lookup Request <h2>Status</h2>Click Here', 'class="nav-button" id="nav-lookup"'); ?>
 </div>
 
-<div class="box nav_button" id="nav_login">
-   <?php echo anchor('auth', 'Manager and Supervisor Login'); ?>
+<div class="grid-item">
+   <?php echo anchor('auth', 'Manage Requests, <h2>Managers</h2>Login Here', 'class="nav-button" id="nav-login"'); ?>
 </div>
